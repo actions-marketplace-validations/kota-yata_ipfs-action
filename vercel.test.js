8a045fd995b5c983207ae5bfee2c449bc9189929
kota-yata <kota@yatagai.com>
2022-01-23 17:53:08 -0600

    remove secret tokens and revoke all of them

--- vercel.test.js
+++ vercel.test.js
@@ -7,10 +7,10 @@ const options = {
   protocol: "https",
   timeout: 1000,
   verbose: true,
-  pinataKey: "1d7d6d3b7bf45ad9b736", // your Pinata key (DO NOT PUSH YOUR CODE WITH YOUR TOKEN!!)
-  pinataSecret: "957c3c265600116576c5c6831c12ef47b677bcd3f7fec8c818d31804cab15209", // your Pinata secret (DO NOT PUSH YOUR CODE WITH YOUR TOKEN!!)
+  pinataKey: "", // your Pinata key (DO NOT PUSH YOUR CODE WITH YOUR TOKEN!!)
+  pinataSecret: "", // your Pinata secret (DO NOT PUSH YOUR CODE WITH YOUR TOKEN!!)
   vercelDomain: "kota-yata.com",
-  vercelToken: "6WHvUh9ohTBBHxnzBIdUKTRi" // your vercel token (DO NOT PUSH YOUR CODE WITH YOUR TOKEN!!)
+  vercelToken: "" // your vercel token (DO NOT PUSH YOUR CODE WITH YOUR TOKEN!!)
 };
 
 uploader.upload({ ...options });
